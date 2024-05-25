a5780a6d08be866b4dfedc4e27d6734d58edadc6
NicolasFluxa <fluxa.andres@gmail.com>
2024-05-25 17:17:05 -0400

    problema resuelto de la mastrices

--- 02Mulriplicacion_matriz.py
+++ 02Mulriplicacion_matriz.py
@@ -2,14 +2,15 @@
 A = [[1,2,3],[4,5,6]]
 B = [[-1,0],[0,1],[1,1]]
 """
-matriz = []
-A = [[1, 2, 3], [4, 5, 6]]
-B = [[-1, 0], [0, 1], [1, 1]]
+import numpy as np
 
-for fila in range(2):
-    matriz.append([])
-    for columna in range(2):
-        matriz[fila].append(columna)
+# Define las matrices
+A = np.array([[1, 2, 3], [4, 5, 6]])
+B = np.array([[-1, 0], [0, 1], [1, 1]])
 
-print(matriz)
+# Multiplica las matrices
+C = np.dot(A, B)
+
+print(C)
+
 
---
+++ 03multiplicacion-op-2.py
@@ -0,0 +1,16 @@
+# Define las matrices
+A = [[1,2,3],[4,5,6]] # Matriz A
+B = [[-1,0],[0,1],[1,1]] # Matriz B
+
+# Crea una matriz vacía para almacenar el resultado
+C = [[0, 0], [0, 0]] # Matriz C que almacenará el resultado de la multiplicación de las matrices A y B
+
+# Realiza la multiplicación de matrices
+for i in range(len(A)): # Recorre cada fila de la matriz A
+   for j in range(len(B[0])): # Recorre cada columna de la matriz B
+       for k in range(len(B)): # Recorre cada fila de la matriz B
+           C[i][j] += A[i][k] * B[k][j] # Multiplica el elemento correspondiente de la matriz A con el elemento correspondiente de la matriz B y lo suma al elemento correspondiente de la matriz C
+
+# Imprime el resultado
+for r in C: # Recorre cada fila de la matriz C
+   print(r) # Imprime la fila
